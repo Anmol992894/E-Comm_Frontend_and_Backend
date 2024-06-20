7c551a9f052cf8104577732c46ade24bcd86747a
Anmol992894 <av992894@gmail.com>
2024-06-20 10:39:47 +0530

    Updated

--- e-commerse/src/index.js
+++ e-commerse/src/index.js
@@ -6,14 +6,23 @@ import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { AppProvider } from './context/product_Context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-8ex5dzydehzipitc.us.auth0.com"
+    clientId="b4UuITl3P3b64p1UannnNwLKWbgTCcFN"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <Provider store={store}>
       <App />
     </Provider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
--- e-commerse/src/pages/Signin.js
+++ e-commerse/src/pages/Signin.js
@@ -7,6 +7,7 @@ import { API_BASE_URL } from '../config';
 import axios from 'axios';
 import Swal from 'sweetalert2'
 import Loading from '../components/Loading.Component';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 
@@ -17,13 +18,49 @@ function SignIn() {
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(true);
     const [loading, setLoading] = useState(false);
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const { user, loginWithRedirect,isAuthenticated,logout} = useAuth0();
+    console.log(user);
+
+    // if (isAuthenticated) {
+    //     logout();
+    // }
+    const signupusingemail = () => {
+        setLoading(true);
+        loginWithRedirect();
+        const requestData = { fullName: fullName, email, password, profileImg: image }
+        // axios.post(`${API_BASE_URL}/signup`, requestData)
+        //     .then((result) => {
+        //         debugger;
+        //         if (result.status == 201) {
+        //             debugger;
+        //             setLoading(false);
+        //             Swal.fire({
+        //                 icon: 'success',
+        //                 title: 'User successfully registered'
+        //             })
+        //         }
+        //         setFullName('');
+        //         setEmail('');
+        //         setPassword('');
+        //         setImage('');
+        //         navigate('/')
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //         setLoading(false);
+        //         Swal.fire({
+        //             icon: 'error',
+        //             title: 'Some error occurred please try again later!'
+        //         })
+        //     })
+    }
 
     const signup = (event) => {
         event.preventDefault();
 
         setLoading(true);
-        const requestData = { fullName: fullName, email, password , profileImg: image }
+        const requestData = { fullName: fullName, email, password, profileImg: image }
         axios.post(`${API_BASE_URL}/signup`, requestData)
             .then((result) => {
                 debugger;
@@ -59,7 +96,7 @@ function SignIn() {
     }, []);
 
     if (isLoading) {
-        return <Loading/>;
+        return <Loading />;
     }
 
     return (
@@ -75,8 +112,8 @@ function SignIn() {
                     </div> : ''}
                     {/* Form for Signin details */}
                     <form onSubmit={(e) => signup(e)}>
-                        <input type="text" value={image} onChange={(ev)=>setImage(ev.target.value)} className="p-2 mt-4 mb-2 form-control input-bg" placeholder='Image URL' />
-                        <input type="email" value={email} onChange={(ev) => setEmail(ev.target.value)}  className="p-2 mb-2 form-control input-bg" placeholder='Email' />
+                        <input type="text" value={image} onChange={(ev) => setImage(ev.target.value)} className="p-2 mt-4 mb-2 form-control input-bg" placeholder='Image URL' />
+                        <input type="email" value={email} onChange={(ev) => setEmail(ev.target.value)} className="p-2 mb-2 form-control input-bg" placeholder='Email' />
                         <input type="text" value={fullName} onChange={(ev) => setFullName(ev.target.value)} className="p-2 mb-2 form-control input-bg" placeholder='Full Name' />
                         <input type="password" value={password} onChange={(ev) => setPassword(ev.target.value)} className="p-2 mb-2 form-control input-bg" placeholder='Password' />
                         <div className='mt-3 d-grid'>
@@ -87,13 +124,19 @@ function SignIn() {
                             <h5 className='text-muted text-center'>OR</h5>
                             <hr className='text-muted' />
                         </div>
-                        <div className='mt-3 mb-5 d-grid'>
-                            <button className="btn">
-                                <span className='fw-bold text-danger fs-6'>Already have an account?</span>
-                                <Link to="/" className='ms-1 text-info fw-bold'>Log In</Link>
-                            </button>
-                        </div>
+
                     </form>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <span className='bg-danger rounded-circle fs-3 p-2 text-light' onClick={e=>signupusingemail()}>
+                            <i class="fa-brands fa-google"></i>
+                        </span>
+                    </div>
+                    <div className='mt-3 mb-5 d-grid'>
+                        <button className="btn">
+                            <span className='fw-bold text-danger fs-6'>Already have an account?</span>
+                            <Link to="/" className='ms-1 text-info fw-bold'>Log In</Link>
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className="foot">
